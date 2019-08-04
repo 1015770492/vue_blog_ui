3e1ef3d921ae4dc1706c459bfae8fc191a493638
花生鼠 <1015770492@qq.com>
2019-08-05 04:41:14 +0800

    初始化提交

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app" class="el-row" >
+    <Login class="login_form"/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld'
+import Login from './components/Login'
+
+export default {
+  name: 'App',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style>
+body{
+  background-image:url('./components/img/bg_login.jpeg')
+}
+  .login_form{
+    background: rgba(255, 255, 255, 0.28);
+    border-radius:10px;
+    position: relative;
+    width: 420px;
+    max-width: 100%;
+    padding: 0px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+  }
+
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div style="margin-top: 140px">
+      <form class="el-row"  action="" method="post">
+        <h2>
+          <span >登&nbsp;&nbsp;&nbsp;&nbsp;录</span>
+        </h2>
+
+      </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "login"
+    }
+</script>
+
+<style scoped>
+
+  .login_form{
+    text-align: center;
+    font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+    color: #f4e8ff;
+  }
+
+</style>
